0f9fe259b57132b9d1e1d2135d86574f94c5cb1e
Elizabeth Kennedy <eakennedy.2412@gmail.com>
2022-10-18 11:31:58 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+"use strict";
+const shortlyInput = document.querySelector(".url-input");
+const shortlyBtn = document.querySelector(".url-button");
+const shortlyResult = document.querySelector(".hidden-result");
+const insertedLink = document.querySelector(".inserted-link");
+const shortCode = document.querySelector(".short-code");
+const parentNode = document.querySelector(".search-result-block");
+const errorMsg = document.querySelector(".error-msg");
+//URL validation
+function urlValidation(defaultUrl) {
+  const urlRule =
+    /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+  if (defaultUrl.match(urlRule)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// URL Submission Click Event
+const apiFunc = shortlyBtn.addEventListener("click", function () {
+  let inputValue = shortlyInput.value;
+  //URL Validation
+  if (!urlValidation(inputValue)) {
+    // alert("Please enter a valid URL!");
+    errorMsg.classList.add("shown");
+    shortlyInput.classList.add("shown");
+    errorMsg.innerHTML = "Please enter a link";
+  } else {
+    errorMsg.classList.remove("shown");
+    shortlyInput.classList.remove("shown");
+    //Passed Validation - init API
+    fetch(`https://api.shrtco.de/v2/shorten?url=` + inputValue)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          let shortlyCode = response.result.code;
+          //Started Cloning
+          let mainClone = shortlyResult.cloneNode(true);
+          mainClone.classList = "search-result";
+          parentNode.appendChild(mainClone);
+          //Finished Cloning
+          //Storage
+          sessionStorage.setItem("cloneCache", parentNode.innerHTML);
+        }
+          //Target clone child elements
+          let cloneLink = mainClone.querySelector(".inserted-link");
+          let cloneResultLink = mainClone.querySelector(".short-code");
+          let cloneCopyBtn = mainClone.querySelector(".copy-btn");
+          //Inserting value of search input
+          cloneLink.textContent = `${inputValue}`;
+
+          //Storage
+          sessionStorage.setItem("cloneLink", cloneLink.textContent);
+          console.log(cloneLink.innerHTML);
+
+          //Inserting the result value
+          cloneResultLink.textContent = `shrtco.de/${shortlyCode}`;
+          //Link Copy Event
+	       const apiFunc = shortlyBtn.addEventListener("click", function () {
+         }
+         )})
